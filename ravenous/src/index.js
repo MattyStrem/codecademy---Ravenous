b989997c538195790c26984cb47d1e0ae7f38c78
Mathew Street <Mathew.Street@lcengineers.co.uk>
2020-05-29 22:52:19 +0100

    Ravenous Part 1 - finished

--- ravenous/src/index.js
+++ ravenous/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './components/App/index.css';
+//import './components/App/App.css';
 import App from './components/App/App';
 import * as serviceWorker from './serviceWorker';
 
