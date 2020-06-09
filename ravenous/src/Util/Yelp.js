18744a4bdade5c693a4e98a54bd3a2b76e60b0aa
Mathew Street <Mathew.Street@lcengineers.co.uk>
2020-06-09 16:31:15 +0100

    Yelp.js Added

---
+++ ravenous/src/Util/Yelp.js
@@ -0,0 +1,39 @@
+const apiKey = "NGdOIsTnZAmczLUhLmq3V91AIot-yvHoYFNeq-QhAtySWjoTawR3eRGDFDlquK_0z-7DtI2yq6kBOzPZ8QRyj73_TXWY6rH39-8qzlOq5Vqk6-PGCZxlJrdUTojfXnYx"
+
+const Yelp = {
+
+    searchYelp(term, location, sortBy) {
+        return fetch(
+            `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}location=${location}&sort_by=${sortBy}`,
+            {
+                headers: {
+                    Authorization: `Bearer ${apiKey}`
+                },
+            }
+        ).then(response => {
+            return response.json();
+        }).then(jsonResponse => {
+            if (jsonResponse.businesses) {
+                return jsonResponse.businesses.map((business) => {
+                    console.log(business);
+                    return {
+                            id: business.id,
+                            imageSrc: business.image_url,
+                            name: business.name,
+                            address: business.location.address1,
+                            city: business.loaction.city,
+                            state: business.location.state,
+                            zipCode: business.location.zipCode,
+                            category: business.categories[0].title,
+                            rating: business.rating,
+                            reviewCount: business.review_count,
+                        }
+
+                });
+            }
+        });
+    }
+}
+
+export default Yelp;
+
